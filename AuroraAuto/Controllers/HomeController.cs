2bd9e0a8043361c4c5db1ac3ba462d04a6d18ae9
shivn06 <shivnaidu06@gmail.com>
2024-04-09 00:56:23 +1200

    customized navbar

--- AuroraAuto/Controllers/HomeController.cs
+++ AuroraAuto/Controllers/HomeController.cs
@@ -22,6 +22,15 @@ namespace AuroraAuto.Controllers
         {
             return View();
         }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
