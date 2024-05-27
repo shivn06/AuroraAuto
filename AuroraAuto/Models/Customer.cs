0cb09103f39bbab4bf5f33f1c08718d33486c970
Shiv Naidu <ac116531@avcol.school.nz>
2024-05-27 12:00:09 +1200

    validations for customer model

--- AuroraAuto/Models/Customer.cs
+++ AuroraAuto/Models/Customer.cs
@@ -1,14 +1,40 @@
-﻿using System.Drawing;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
 
 namespace AuroraAuto.Models
 {
     public class Customer
     {
         public int CustomerID { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [DisplayName("First Name")]
+        [RegularExpression("^[A-Za-z]+$", ErrorMessage = "You have entered an Invalid Character")]
         public required string FirstName { get; set; }
+
+        [StringLength(30)]
+        [DisplayName("Middle Name(s)")]
+        public string? MiddleName { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [DisplayName("Last Name")]
+        [RegularExpression("^[A-Za-z]+$", ErrorMessage = "You have entered an Invalid Character")]
         public required string LastName { get; set; }
+
+        [Required]
         public required string Address { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression("^(((\\+?64\\s*[-\\.\\ ]?[3-9]|\\(?0[3-9]\\)?)\\s*[-\\.\\ ]?\\d{3}\\s*[-\\.\\ ]?\\d{4})|((\\+?64\\s*[-\\.\\(\\ ]?2\\d{1,2}[-\\.\\)\\ ]?|\\(?02\\d{1}\\)?)\\s*[-\\.\\ ]?\\d{3}\\s*[-\\.\\ ]?\\d{3,5})|((\\+?64\\s*[-\\.\\ ]?[-\\.\\(\\ ]?800[-\\.\\)\\ ]?|[-\\.\\(\\ ]?0800[-\\.\\)\\ ]?)\\s*[-\\.\\ ]?\\d{3}\\s*[-\\.\\ ]?(\\d{2}|\\d{5})))|^$$", ErrorMessage = "Please enter a valid Phone Number")]
         public required string Phone { get; set; }
+
+        [Required]
+        [DisplayName("Email")]
+        [DataType(DataType.EmailAddress)]
         public required string Email { get; set; }
 
         public required ICollection<Order> Orders { get; set; }
