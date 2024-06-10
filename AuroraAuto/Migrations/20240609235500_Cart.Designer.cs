fbe4bd9816e0100c9b6c1511420e1de13c350e61
Shiv Naidu <ac116531@avcol.school.nz>
2024-06-10 12:00:10 +1200

    Created Carts Controller

--- AuroraAuto/Migrations/20240609235500_Cart.Designer.cs
+++ AuroraAuto/Migrations/20240609235500_Cart.Designer.cs
@@ -4,6 +4,7 @@ using AuroraAuto.Areas.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AuroraAuto.Migrations
 {
     [DbContext(typeof(AuroraAutoContext))]
-    partial class AuroraAutoContextModelSnapshot : ModelSnapshot
+    [Migration("20240609235500_Cart")]
+    partial class Cart
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
