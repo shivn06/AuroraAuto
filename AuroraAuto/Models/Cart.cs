dbad102404f159cd434963acc0987261f30bc6ca
Shiv Naidu <ac116531@avcol.school.nz>
2024-07-31 10:03:01 +1200

    added regex and validations to cart model

--- AuroraAuto/Models/Cart.cs
+++ AuroraAuto/Models/Cart.cs
@@ -1,11 +1,24 @@
-﻿namespace AuroraAuto.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuroraAuto.Models
 {
     public class Cart
     {
         public int CartID { get; set; }
+
+        [DisplayName("Quantity")]
+        [RegularExpression("^\\s*-?\\d{1,3}(\\.\\d{1,4})?\\s*$", ErrorMessage = "You have entered an incorrect value")]
         public int? Quantity { get; set; }
+
+        [DisplayName("Total Price")]
+        [RegularExpression("^\\s*-?\\d{1,3}(\\.\\d{1,4})?\\s*$", ErrorMessage = "You have entered an incorrect value")]
         public int? TotalPrice { get; set; }
+
+        [DisplayName("Customer")]
         public int? CustomerID { get; set; }
+
+        [DisplayName("Product Name")]
         public int? ProductID { get; set; }
 
         public Customer Customer { get; set; } = null!;
