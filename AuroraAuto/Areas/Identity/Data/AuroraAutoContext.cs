dc9997be9c4d43208aa3bda0b48f3854d9b6f8b3
Shiv Naidu <ac116531@avcol.school.nz>
2024-08-29 15:14:34 +1200

    database

--- AuroraAuto/Areas/Identity/Data/AuroraAutoContext.cs
+++ AuroraAuto/Areas/Identity/Data/AuroraAutoContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using AuroraAuto.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace AuroraAuto.Areas.Identity.Data;
 
@@ -18,6 +19,8 @@ public class AuroraAutoContext : IdentityDbContext<IdentityUser>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
     }
 
 public DbSet<AuroraAuto.Models.Customer> Customer { get; set; } = default!;
@@ -33,4 +36,14 @@ public DbSet<AuroraAuto.Models.Payment> Payment { get; set; } = default!;
 public DbSet<AuroraAuto.Models.Product> Product { get; set; } = default!;
 
 
+
+    public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<AuroraAutoUser>
+    {
+        public void Configure(EntityTypeBuilder<AuroraAutoUser> builder)
+
+        {
+            builder.Property(u => u.FirstName).HasMaxLength(20);
+            builder.Property(u => u.LastName).HasMaxLength(20);
+        }
+    }
 }
